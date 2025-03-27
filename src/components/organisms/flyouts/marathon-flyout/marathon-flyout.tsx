ecb19052b4c90efb14f18afe1df91e5256863f09
Xavier Fernandes <xavier.fernandes@vwds.pt>
2025-03-27 19:57:06 +0000

    🧵 populate marathon flyout with a list of districts

--- src/components/organisms/flyouts/marathon-flyout/marathon-flyout.tsx
+++ src/components/organisms/flyouts/marathon-flyout/marathon-flyout.tsx
@@ -1,12 +1,20 @@
 import { Flyout } from '../../../molecules/flyout/flyout.tsx';
 import { FlyoutProps } from './marathon-flyout.props.ts';
+import { useMunicipalities } from '../../../../providers/municipalities-provider.tsx';
+import './marathon-flyout.css';
 
 export function MarathonFlyout({ isOpen }: FlyoutProps) {
+    const { districts } = useMunicipalities();
+
     return (
         <Flyout isOpen={isOpen}>
-            <h1>Olá, eu sou um flyout!</h1>
-            <p>Este é um componente de exemplo que pode ser utilizado para exibir informações adicionais ou ações em uma
-                tela.</p>
+            <h1>CONCELHOS POR DISTRITO</h1>
+            <p>Aqui podes ver que concelhos já acertaste para cada um dos 18 distritos.</p>
+
+            <ul className="marathon-flyout-container">
+                {Array.from(districts).map((district) => <li className="marathon-flyout-item">{district}</li>)}
+
+            </ul>
         </Flyout>
     );
 }
