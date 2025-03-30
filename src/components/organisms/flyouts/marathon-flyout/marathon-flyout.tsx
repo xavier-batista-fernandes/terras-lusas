d34b9bf89082aa6b6ff2cef40dfc044c19f5b265
Xavier Fernandes <xavier.fernandes@vwds.pt>
2025-03-31 00:19:48 +0100

    🪓 display municipalities under each district in marathon flyout

--- src/components/organisms/flyouts/marathon-flyout/marathon-flyout.tsx
+++ src/components/organisms/flyouts/marathon-flyout/marathon-flyout.tsx
@@ -2,19 +2,38 @@ import { Flyout } from '../../../molecules/flyout/flyout.tsx';
 import { FlyoutProps } from './marathon-flyout.props.ts';
 import { useMunicipalities } from '../../../../providers/municipalities-provider.tsx';
 import './marathon-flyout.css';
+import { useEffect } from 'react';
 
 export function MarathonFlyout({ isOpen }: FlyoutProps) {
-    const { districts } = useMunicipalities();
+    const { districts, municipalities } = useMunicipalities();
+
+    useEffect(() => {
+        // TODO: protect component against data not being available
+    }, []);
 
     return (
         <Flyout isOpen={isOpen}>
             <h1>CONCELHOS POR DISTRITO</h1>
             <p>Aqui podes ver que concelhos já acertaste por distrito.</p>
 
-            <ul className="marathon-flyout-container">
-                {Array.from(districts).map((district) =>
-                    <li key={district} className="marathon-flyout-item">{district}</li>)}
-            </ul>
+            <div className="marathon-flyout-container">
+                <ul className="marathon-flyout-districts-list">
+                    {Array.from(districts).map((district) =>
+                        <li key={district}
+                            className="marathon-flyout-districts-item"
+                        >
+                            <h2>{district}</h2>
+                            <ul className="marathon-flyout-municipalities-list">
+                                {Array.from(municipalities.get(district)!.values()).map(municipality =>
+                                    <li key={municipality}
+                                        className="marathon-flyout-municipalities-item"
+                                    >
+                                        {municipality}
+                                    </li>)}
+                            </ul>
+                        </li>)}
+                </ul>
+            </div>
         </Flyout>
     );
 }
