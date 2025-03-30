ef05bc4818a7d0bbbfa0e8a88684919619f1245e
Xavier Fernandes <xavier.fernandes@vwds.pt>
2025-03-30 23:39:53 +0100

    🔑 improve data structure by adding municipalities to context

--- src/providers/municipalities-provider.tsx
+++ src/providers/municipalities-provider.tsx
@@ -5,9 +5,10 @@ import { toTitleCase } from '../utilities/toTitleCase.ts';
 const MunicipalitiesContext = createContext<MunicipalitiesContextData | undefined>(undefined);
 
 export interface MunicipalitiesContextData {
-    rawData?: any;
-    districts: Set<string>;
     isLoading: boolean;
+    rawData?: any; // TODO: abstract raw data from the rest of the app
+    districts: Set<string>;
+    municipalities: Map<string, Set<string>>;
 }
 
 export const useMunicipalities = () => {
@@ -18,14 +19,17 @@ export const useMunicipalities = () => {
 
 export const MunicipalitiesProvider = ({ children }: { children: ReactNode }) => {
 
-    const [rawData, setRawData] = useState<{ type: string, features: any } | undefined>(undefined);
-    const [districts, setDistricts] = useState(new Set<string>());
     const [isLoading, setIsLoading] = useState(true);
+    const [rawData, setRawData] = useState<{ type: string, features: any } | undefined>(undefined);
+
+    const [districts, setDistricts] = useState(new Set<string>());
+    const [municipalities, setMunicipalities] = useState(new Map<string, Set<string>>());
 
     const data: MunicipalitiesContextData = {
+        isLoading,
         rawData,
         districts,
-        isLoading,
+        municipalities,
     };
 
     useEffect(() => {
@@ -37,13 +41,27 @@ export const MunicipalitiesProvider = ({ children }: { children: ReactNode }) =>
             setRawData(rawData);
 
             // Extract districts from the raw data
-            const districts = new Set<string>(rawData.features.map((municipality: any) =>
-                toTitleCase(municipality.properties['District'])));
-            console.log('districts', districts);
-            setDistricts(districts);
+            const newMunicipalities = new Map<string, Set<string>>();
 
-            // Loading is complete
+            rawData.features.forEach(
+                (feature: any) => {
+                    const district = toTitleCase(feature.properties['District']);
+                    const municipality = toTitleCase(feature.properties['Municipality']);
+
+                    // Create a set for the district if it doesn't exist
+                    if (!newMunicipalities.get(district)) {
+                        newMunicipalities.set(district, new Set<string>());
+                    }
+
+                    // Add municipality to the district set
+                    newMunicipalities.get(district)!.add(municipality);
+                },
+            );
+
+            setDistricts(new Set(newMunicipalities.keys()));
+            setMunicipalities(new Map(newMunicipalities));
             setIsLoading(false);
+
             // TODO: handle errors and allow for page reloads
         };
 
