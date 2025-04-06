a7063d13c77d9bbd667f61b538a40b82bcf71d0a
Xavier Fernandes <xavier.fernandes@vwds.pt>
2025-04-06 11:45:30 +0100

    🪀 display municipalities in marathon flyout as they are guessed

--- src/components/organisms/flyouts/marathon-flyout/marathon-flyout.tsx
+++ src/components/organisms/flyouts/marathon-flyout/marathon-flyout.tsx
@@ -2,19 +2,17 @@ import './marathon-flyout.css';
 import { Flyout } from '../../../molecules/flyout/flyout.tsx';
 import { FlyoutProps } from './marathon-flyout.props.ts';
 import { useMunicipalities } from '../../../../providers/municipalities-provider.tsx';
-import { useEffect } from 'react';
+import { useMarathon } from '../../../../providers/marathon-provider.tsx';
 
 export function MarathonFlyout({ isOpen }: FlyoutProps) {
     const { districts, municipalitiesPerDistrict } = useMunicipalities();
-    useEffect(() => {
-        // TODO: protect component against data not being available
-    }, [isOpen]);
+    const { guessedMunicipalities } = useMarathon();
 
     return (
         <Flyout isOpen={isOpen}>
             <h1>CONCELHOS POR DISTRITO</h1>
             <p>Aqui podes ver que concelhos já acertaste por distrito.</p>
-            <button onClick={() => isOpen}>X</button>
+            {/*<button onClick={() => isOpen}>X</button>*/}
 
             <div className="marathon-flyout-container">
                 <ul className="marathon-flyout-districts-list">
@@ -24,12 +22,14 @@ export function MarathonFlyout({ isOpen }: FlyoutProps) {
                         >
                             <h2>{district}</h2>
                             <ul className="marathon-flyout-municipalities-list">
-                                {Array.from(municipalitiesPerDistrict.get(district)!.values()).map(municipality =>
-                                    <li key={municipality}
-                                        className="marathon-flyout-municipalities-item"
-                                    >
-                                        {municipality}
-                                    </li>)}
+                                {Array.from(municipalitiesPerDistrict.get(district)!.values()).map(
+                                    municipality =>
+                                        <li key={municipality}
+                                            className="marathon-flyout-municipalities-item"
+                                        >
+                                            {guessedMunicipalities.has(municipality.toLowerCase()) && municipality}
+                                        </li>,
+                                )}
                             </ul>
                         </li>)}
                 </ul>
