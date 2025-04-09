b8518c567b32b0cc8f65e3ce8dd996eb751a38c4
Xavier Fernandes <xavier.fernandes@vwds.pt>
2025-04-09 18:58:43 +0100

    🐥 update base path

--- src/app.tsx
+++ src/app.tsx
@@ -10,7 +10,7 @@ export const App = () => {
     return (
         <>
             <MunicipalitiesProvider>
-                <BrowserRouter basename={'/TERRAS-LUSAS'}>
+                <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route
