c966d0541f7403b68f712deb1d12a738707e4405
Xavier Fernandes <xavier.fernandes@vwds.pt>
2025-04-21 00:12:49 +0100

    🪣 update hostname in sitemap plugin configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import SitemapPlugin from 'vite-plugin-sitemap';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), SitemapPlugin()],
+    plugins: [
+        react(),
+        Sitemap({ hostname: 'https://terras-lusas.vercel.app' }),
+    ],
 });
