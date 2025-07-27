a8f668004e1375683304dad3459af28475806248
Xavier Fernandes <xavierbatistafernandes.001@gmail.com>
2025-07-27 21:36:43 +0100

    🧢 add tailwindcss to the dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        tailwindcss(),
         Sitemap({ hostname: 'https://terras-lusas.vercel.app' }),
     ],
 });
